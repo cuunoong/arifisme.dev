8b68920b0f0ce5ab633b01d0bc67ea49c7cb389e
Arif Iskandar <arif19iskandar@gmail.com>
2022-08-19 01:18:29 +0700

    Fixing next seo images

--- configs/next-seo.config.ts
+++ configs/next-seo.config.ts
@@ -1,16 +1,18 @@
-import id from '../lang/id'
+import { DefaultSeoProps } from 'next-seo'
 
 export default {
+  defaultOpenGraphImageWidth: 800,
+  canonical: process.env.NEXT_PUBLIC_APP_URL,
   openGraph: {
     type: 'website',
     locale: 'id_ID',
-    site_name: id.title,
-    image: `${process.env.NEXT_PUBLIC_APP_URL}/og-image.jpg`,
+    site_name: 'SiteName',
+    url: process.env.NEXT_PUBLIC_APP_URL,
+    images: [{ url: `${process.env.NEXT_PUBLIC_APP_URL}/og-image.jpg` }],
   },
   twitter: {
     handle: '@cuunoongs',
     site: '@cuunoongs',
     cardType: 'summary_large_image',
-    image: `${process.env.NEXT_PUBLIC_APP_URL}/og-image.jpg`,
   },
-}
+} as DefaultSeoProps
