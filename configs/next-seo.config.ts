7127f8dda04c9678acc46f324759b10335744560
Arif Iskandar <arif19iskandar@gmail.com>
2022-08-18 23:31:20 +0700

    Adding seo image

--- configs/next-seo.config.ts
+++ configs/next-seo.config.ts
@@ -1,12 +1,16 @@
+import id from '../lang/id'
+
 export default {
   openGraph: {
     type: 'website',
-    locale: 'en_IE',
-    site_name: "Let's learn to code an application - Arif Iskandar",
+    locale: 'id_ID',
+    site_name: id.title,
+    image: `${process.env.NEXT_PUBLIC_APP_URL}/og-image.jpg`,
   },
   twitter: {
     handle: '@cuunoongs',
     site: '@cuunoongs',
     cardType: 'summary_large_image',
+    image: `${process.env.NEXT_PUBLIC_APP_URL}/og-image.jpg`,
   },
 }
