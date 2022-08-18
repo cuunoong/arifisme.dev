844d3792a07b08731391a5a7fd74d097f5691470
Arif Iskandar <arif19iskandar@gmail.com>
2022-08-18 22:45:13 +0700

    Adding analytic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,48 @@ import SEO from '../configs/next-seo.config'
 import Cursor from '../components/cursor'
 import { appWithTranslation } from 'next-i18next'
 import translate from '../utils/translate'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
+import * as gtag from '../utils/analytic'
+import Script from 'next/script'
+
 function MyApp({ Component, pageProps, router: { locale } }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
       <Head>
         <title>{translate(locale).title}</title>
         <link rel="icon" href="/favicon.ico" />
