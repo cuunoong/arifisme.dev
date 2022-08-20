3778b11c39d8e208e4242f52a585a03cb20422b0
Arif Iskandar <arif19iskandar@gmail.com>
2022-08-20 11:22:23 +0700

    Fixing lessons grid count

--- components/lessons.tsx
+++ components/lessons.tsx
@@ -34,7 +34,7 @@ function Lessons({
       className="mb-9 flex flex-col items-center space-y-9 py-12"
       id="lessons"
     >
-      <div className="mx-auto grid w-full gap-3 px-6 py-8 md:max-w-2xl md:grid-cols-2 md:px-2  lg:grid-cols-3 xl:max-w-7xl xl:gap-9">
+      <div className="mx-auto grid w-full gap-3 px-6 py-8 md:max-w-2xl md:grid-cols-2 md:px-2  xl:max-w-7xl xl:grid-cols-3 xl:gap-9">
         {lessons.map((lesson, index) => (
           <Card key={index} lesson={lesson} />
         ))}
