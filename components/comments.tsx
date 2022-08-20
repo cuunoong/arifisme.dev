d6f4e490f8c3c1a00e48130550adcedc41148d59
Arif Iskandar <arif19iskandar@gmail.com>
2022-08-20 11:21:50 +0700

    Fixing comments layout

--- components/comments.tsx
+++ components/comments.tsx
@@ -5,15 +5,43 @@ import Comment from './comment'
 function Comments({ comments }: { comments: CommentData[] }) {
   return (
     <section className="relative mx-auto mt-16  px-6  md:mt-24 md:max-w-2xl md:px-2  xl:max-w-7xl ">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-3 xl:gap-8">
-        <div className="block  space-y-4 md:space-y-6 lg:space-y-8">
+      <div className="hidden grid-cols-3 gap-8 xl:grid">
+        <div className="space-y-8">
           <GetComments comments={comments} index={0} />
         </div>
-        <div className="hidden space-y-4 md:block md:space-y-6 lg:space-y-8 ">
+        <div className="space-y-8">
           <GetComments comments={comments} index={1} />
         </div>
-        <div className="hidden space-y-4 md:space-y-6 lg:space-y-8 xl:block ">
+        <div className="space-y-8">
           <GetComments comments={comments} index={2} />
+        </div>
+      </div>
+      <div className="hidden grid-cols-2 gap-6  md:grid xl:hidden">
+        <div className="space-y-6">
+          <GetComments
+            comments={comments}
+            index={0}
+            itemCount={4}
+            addition={2}
+          />
+        </div>
+        <div className="space-y-6">
+          <GetComments
+            comments={comments}
+            index={1}
+            itemCount={4}
+            addition={2}
+          />
+        </div>
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:hidden">
+        <div className="space-y-4">
+          <GetComments
+            comments={comments}
+            index={0}
+            itemCount={4}
+            addition={1}
+          />
         </div>
       </div>
       <div className="pointer-events-none absolute inset-x-0 bottom-0 flex justify-center bg-gradient-to-t from-white pt-32 pb-8 dark:from-black "></div>
@@ -24,14 +52,18 @@ function Comments({ comments }: { comments: CommentData[] }) {
 const GetComments = ({
   index,
   comments,
+  addition = 3,
+  itemCount = 3,
 }: {
   index: number
+  addition?: number
+  itemCount?: number
   comments: CommentData[]
 }) => (
   <>
-    <Comment comment={comments[index]} />
-    <Comment comment={comments[index + 3]} />
-    <Comment comment={comments[index + 6]} />
+    {Array.from(Array(itemCount).keys()).map((_, id) => (
+      <Comment comment={comments[index + id * addition]} key={id} />
+    ))}
   </>
 )
 
