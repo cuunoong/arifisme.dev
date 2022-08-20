04c0c7d93f3579e326fbf6ae9efaa20fa5afa18a
Arif Iskandar <arif19iskandar@gmail.com>
2022-08-20 11:31:16 +0700

    Disable anation on hash target

--- anim/index.ts
+++ anim/index.ts
@@ -1,11 +1,18 @@
 import { gsap } from 'gsap'
 import detail from './detail'
+import footer from './footer'
+import header from './header'
 import homepage from './homepage'
 
 export default function animation(page: 'home' | 'detail') {
   // prevent flashing
   gsap.to('html, body', { css: { visibility: 'visible' }, duration: 0 })
 
-  if (page == 'home') homepage()
-  else detail()
+  if (window.location.hash) {
+    header()
+    footer()
+  } else {
+    if (page == 'home') homepage()
+    else detail()
+  }
 }
